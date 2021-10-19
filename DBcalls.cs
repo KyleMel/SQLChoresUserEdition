0c470b19208309aa37fd6d513c3c6bba9161988d
Kyle Melton <87396239+KyleMel@users.noreply.github.com>
2021-10-19 17:15:57 -0500

    "Original Project https://github.com/KyleMel/SQLChores.git"
    Based off SQLChores project demonstrating using SQL with C#
    This version adds functionality of INSERTING, DELETING, UPDATING,
    and SELECT * from a user defined table.

---
+++ DBcalls.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace SQLChores
+{
+    class DBcalls
+    {
+        private string connectionString;
+        private string sql;
+        public DBcalls(string connection)
+        {
+            connectionString = connection;
+        }
+        public void CreateDB()
+        {
+            Console.Write("Creating Chore DataBase...");
+            string sql = "DROP DATABASE IF EXISTS [ChoresDB]; CREATE DATABASE [ChoresDB]";
+            RunQuery(sql);
+        }
+        public void CreateTable()
+        {
+            Console.Write("Creating Chores Table...");
+            StringBuilder sb = new StringBuilder();
+            sb.Append("USE ChoresDB; ");
+            sb.Append("CREATE TABLE Chores ( ");
+            sb.Append("ChoreID INT IDENTITY(1,1) NOT NULL PRIMARY KEY, ");
+            sb.Append("ChoreName NVARCHAR(50),");
+            sb.Append("ChoreAssignment NVARCHAR(50)); ");
+            sql = sb.ToString();
+            RunQuery(sql);
+        }
+        public void AddChore(string choreName, string choreAssignment)
+        {
+            Console.Write("Adding Chore...");
+            StringBuilder sb = new StringBuilder();
+            sb.Append("USE ChoresDB; ");
+            sb.Append("INSERT INTO Chores (ChoreName, ChoreAssignment) VALUES");
+            sb.Append($"('{choreName}', '{choreAssignment}');");
+            sql = sb.ToString();
+            RunQuery(sql);
+        }
+        public void UpdateChore(string choreAssignment, string choreName)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("USE ChoresDB; ");
+            sb.Append($"UPDATE Chores SET ChoreName = '{choreName}' ");
+            sb.Append($"WHERE ChoreAssignment = '{choreAssignment}'; ");
+            sql = sb.ToString();
+            RunQuery(sql);
+        }
+        public void DeleteChore(int userID)
+        {
+            Console.WriteLine("Deleting Chore...");
+            sql = $"USE ChoresDB DELETE FROM Chores WHERE ChoreID = {userID};";
+            RunQuery(sql);
+        }
+        public void GetChore()
+        {
+            Console.WriteLine("Listing Chores...");
+            sql = "USE ChoresDB SELECT * FROM Chores;";
+            ListQuery(sql);
+        }
+
+        public void RunQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    Console.WriteLine(" Done");
+                }
+            }
+        }
+        public void ListQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("|{0}|{1}|{2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace SQLChores
+{
+    class User
+    {
+        private string userChoreName;
+        private string userAssignName;
+        private int userID;
+        private int inputString;
+        private static string login;
+
+        public User(int input)
+        {
+            inputString = input;
+        }
+        public static void Welcome()
+        {
+            login = "Data Source=localhost;Initial Catalog=master;User ID=Kyle;Password=Temp123!";
+            var calls = new DBcalls(login);
+            calls.CreateDB();
+            calls.CreateTable();
+            Console.WriteLine("Please Select What You Would Like To Do:");
+            while (true)
+            {
+                Console.Write(" 1. Add Chore\n 2. Delete Chore\n 3. Update Chore\n 4. List All\n 5. Exit\nEnter: ");
+                try
+                {
+                    int userInput = Convert.ToInt32(Console.ReadLine());
+                    var user = new User(userInput);
+                    user.UserCases();
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                Console.Write("Back To Main Menu: Y/N\nEnter: ");
+                var response = Console.ReadLine();
+                Console.Clear();
+                if (response == "n") break;
+            }
+        }
+        public void UserCases()
+        {
+            var calls = new DBcalls(login);
+            try
+            {
+                switch (inputString)
+                {
+                    case 1:
+                        UserAddChore();
+                        calls.AddChore(userChoreName, userAssignName);
+                        break;
+                    case 2:
+                        calls.GetChore();
+                        UserDeleteChore();
+                        calls.DeleteChore(userID);
+                        break;
+                    case 3:
+                        calls.GetChore();
+                        UserUpdateChore();
+                        calls.UpdateChore(userAssignName, userChoreName);
+                        break;
+                    case 4:
+                        calls.GetChore();
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public void UserAddChore()
+        {
+            Console.Write("Enter a Chore: ");
+            userChoreName = Console.ReadLine();
+            Console.Write("Enter who to assign it to: ");
+            userAssignName = Console.ReadLine();
+        }
+        public void UserUpdateChore()
+        {
+            Console.Write("Select Person to Reassign: ");
+            userAssignName = Console.ReadLine();
+            Console.Write("Enter New Task: ");
+            userChoreName = Console.ReadLine();
+        }
+        public void UserDeleteChore()
+        {
+            Console.Write("Select Person ID to Delete: ");
+            userID = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
